ba8fdf1cb2e307250b9df83c4a2cd0fb843a18ce
Reza Jahromi <62157176+rezajahromi@users.noreply.github.com>
2021-10-10 13:02:32 -0500

    complete

---
+++ rottenpotatoes/spec/factories/movies.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :movie do
+    title  {'xxx' }
+    rating  {'G'}
+    release_date {'25-Nov-1993'}
+    
+    trait :with_director do
+        director {'yes'}
+    end
+    
+end
+
+end
